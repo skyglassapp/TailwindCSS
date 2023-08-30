43091ce95165df427102fecdacd325ba9c995207
Ryan Burgoyne <ryan@skyglass.com>
2023-08-30 17:22:45 -0600

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
+            name: "tailwindcss",
             url: "https://api.github.com/repos/skyglassapp/TailwindCSS/releases/assets/123847309.zip",
             checksum: "66f46b379b01e983b8334fa2fef9f9c2d3f96bc404ed71c5b65b1d95e25dcdfa"
         ),
